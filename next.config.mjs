34d6a04f66ff614c9888374bd639b26fa884d713
BaltaMolteni <baltazarmolteni04@gmail.com>
2025-03-05 18:41:20 -0300

    Config para gh pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: "export",
+    basePath: "/Portfolio", 
+    assetPrefix: "/Portfolio/"
+  };
+  
+  module.exports = nextConfig;
+  
